e95dced4ead0c2d93a7892d32ade4a4bee293591
Rico <r@rico.wtf>
2025-07-13 08:55:44 +0200

    passing tests

--- test/pled/decoder_test.exs
+++ test/pled/decoder_test.exs
@@ -3,14 +3,46 @@ defmodule Pled.DecoderTest do
 
   alias Pled.Commands.Encoder
 
-  describe "encode/1" do
+  describe "encode functions" do
     setup do
-      temp_dir = System.tmp_dir!()
+      temp_dir = System.tmp_dir!() |> Path.join("pled_test_#{System.unique_integer([:positive])}")
+      File.mkdir_p!(temp_dir)
+
+      File.cp("priv/examples/single_element.json", Path.join(temp_dir, "AAC.json"))
+      File.write(Path.join(temp_dir, ".key"), "AAC")
+
+      File.write(temp_dir |> Path.join("initialize.js"), "console.log('this is a test')")
+      File.write(temp_dir |> Path.join("update.js"), "console.log('this is a test')")
+      File.write(temp_dir |> Path.join("reset.js"), "console.log('this is a test')")
+      File.write(temp_dir |> Path.join("preview.js"), "console.log('this is a test')")
+
+      on_exit(fn ->
+        # Clean up the entire temporary directory
+        if File.exists?(temp_dir) do
+          File.rm_rf!(temp_dir)
+        end
+      end)
+
       {:ok, %{temp_dir: temp_dir}}
     end
 
+    test "encode_element/1", %{temp_dir: temp_dir} do
+      encoded_element = Encoder.encode_element(temp_dir)
+      assert is_tuple(encoded_element)
+      {key, value} = encoded_element
+      assert key == "AAC"
+      assert is_map(value["code"])
+      assert Map.keys(value["code"]["initialize"])
+    end
+
+    test "generate_code_block/1", %{temp_dir: temp_dir} do
+      generated_code = Encoder.generate_code_block(temp_dir)
+      assert is_map(generated_code)
+      assert Map.keys(generated_code) == ["code"]
+      assert Map.keys(generated_code["code"]) == ["initialize", "preview", "reset", "update"]
+    end
+
     test "generate_js_file/2 :initialize", %{temp_dir: temp_dir} do
-      File.write(temp_dir |> Path.join("initialize.js"), "console.log('this is a test')")
       generated_map = Encoder.generate_js_file(:initialize, temp_dir)
       assert is_map(generated_map)
       assert Map.keys(generated_map) == ["initialize"]
@@ -22,7 +54,6 @@ defmodule Pled.DecoderTest do
     end
 
     test "generate_js_file/2 :update", %{temp_dir: temp_dir} do
-      File.write(temp_dir |> Path.join("update.js"), "console.log('this is a test')")
       generated_map = Encoder.generate_js_file(:update, temp_dir)
       assert is_map(generated_map)
       assert Map.keys(generated_map) == ["update"]
@@ -34,7 +65,6 @@ defmodule Pled.DecoderTest do
     end
 
     test "generate_js_file/2 :reset", %{temp_dir: temp_dir} do
-      File.write(temp_dir |> Path.join("reset.js"), "console.log('this is a test')")
       generated_map = Encoder.generate_js_file(:reset, temp_dir)
       assert is_map(generated_map)
       assert Map.keys(generated_map) == ["reset"]
@@ -46,7 +76,6 @@ defmodule Pled.DecoderTest do
     end
 
     test "generate_js_file/2 :preview", %{temp_dir: temp_dir} do
-      File.write(temp_dir |> Path.join("preview.js"), "console.log('this is a test')")
       generated_map = Encoder.generate_js_file(:preview, temp_dir)
       assert is_map(generated_map)
       assert Map.keys(generated_map) == ["preview"]
