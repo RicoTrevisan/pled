4b13f86a7d48b58246571bea9bb6fba4a4ccfe6b
Rico <r@rico.wtf>
2025-08-14 13:36:16 +0200

    improved llm text

--- lib/pled/commands/init.ex
+++ lib/pled/commands/init.ex
@@ -84,7 +84,10 @@ defmodule Pled.Commands.Init do
 
     - when changing `lib/index.js`, run `npm run build` in the `lib/` directory, then rename the `lib/dist.js` file to the latest version (start at `dist-v01.js` and go up from there), the run `pled upload lib/dist-vVERSION.js`
     - when making changes to an element's `initialize.js` or `update.js`, in order to verify if the changes are working, use the Playwright MCP server and open the page listed in env var "TEST_URL"
-
+    - in initialize and update, you never have to add the standard bubble `function(properties...)`. Pled will do that automatically.
+    - `initialize.js` runs with `instance` and `context`
+    - `update.js` runs with `instance`, `properties`, and `context`
+    - if a `shared_keys` is `secure`, it is never available in the elements, only in server-side actions.
 
     """
 
